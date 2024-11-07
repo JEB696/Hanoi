0250e41f0b70c13fe2a92139da5de26f0aab8ede
John.B <John.B@LAPTOP-JOLK9B6Q.home>
2024-11-06 20:36:08 -0500

    having trouble javadocing it mainly commented

--- ICS4U-ExampleCode/src/unit2StarterCode/Search.java
+++ ICS4U-ExampleCode/src/unit2StarterCode/Search.java
@@ -1,58 +1,107 @@
 /**
- * 
- */
+*
+*/
 package unit2StarterCode;
-
 import java.util.Scanner;
-
 /**
  * 
  */
+/**
+ * The Search class provides methods for searching a number in an array.
+ * It includes implementations for linear and binary search algorithms, 
+ * as well as counters to track the number of loops and comparisons made during the search.
+ */
 public class Search {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		//Get array values from the user
-		System.out.print("How many numbers would you like: ");
-		int n = sc.nextInt();
-		int[] array = new int[n];
-		
-		System.out.println("Please enter the numbers one at a time.");
-		for(int i = 0; i < n; i++) {
-			array[i] = sc.nextInt();
-		}
-		
-		System.out.println("What value would you like to search for?");
-		int num = sc.nextInt();
-		
-		//sort the array if needed using your Sort class
-		//Sort.quick(array);
-		int index = linear(array, num);//call your search method here
-		System.out.println("Your number, " + num +", was found at index " + index);
-		
-		sc.close();
+    // Counter for the number of loops executed
+    public static long loopCounter;
 
-	}
-	
-	/**
-	 * Add comments
-	 */
-	public static int linear(int[] arr, int target) {
-		//TODO write the linear search method
-		return 0;
-	}
-	
-	/**
-	 * Add comments
-	 */
-	public static int binary(int[] arr, int target) {
-		//TODO write the binary search method
-		//https://www.youtube.com/watch?v=MFhxShGxHWc
-		return 0;
-	}
+    // Counter for the number of comparisons made
+    public static long comparisonCounter;
 
+    /**
+     * The main method prompts the user for input, calls the appropriate search method, 
+     * and displays the result.
+     * 
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Get array values from the user
+        System.out.print("How many numbers would you like: ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Please enter the numbers one at a time.");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("What value would you like to search for?");
+        int num = sc.nextInt();
+
+        // Sort the array if needed using your Sort class (currently commented out)
+        // Sort.quick(array);
+
+        // Call the linear search method
+        int index = linear(array, num);
+
+        // Output the result of the search
+        System.out.println("Your number, " + num + ", was found at index " + index);
+
+        sc.close();
+    }
+
+    /**
+     * Performs a linear search on the given array to find the target value.
+     * This method counts the number of loop iterations and comparisons during the search.
+     *
+     * @param arr The array to search
+     * @param target The value to search for
+     * @return The index of the target value if found, otherwise -1
+     */
+    public static int linear(int[] arr, int target) {
+        // Linear search through the array
+        for (int i = 0; i < arr.length; i++) {
+            Search.loopCounter++; // Increment the loop counter for each iteration
+            if (arr[i] == target) {
+                Search.comparisonCounter++; // Increment the comparison counter when a comparison occurs
+                return i; // Return the index where the target is found
+            }
+        }
+        return -1; // Return -1 if the target is not found in the array
+    }
+
+    /**
+     * Performs a binary search on the given sorted array to find the target value.
+     * This method counts the number of loop iterations and comparisons during the search.
+     *
+     * @param arr The sorted array to search
+     * @param target The value to search for
+     * @return The index of the target value if found, otherwise -1
+     */
+    public static int binary(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        // Binary search through the array
+        while (low <= high) {
+            Search.loopCounter++; // Increment the loop counter for each iteration
+            int mid = low + (high - low) / 2;
+
+            // Perform comparisons and adjust search range based on target comparison
+            if (arr[mid] == target) {
+                Search.comparisonCounter++; // Increment the comparison counter
+                return mid; // Return the index where the target is found
+            } else if (arr[mid] < target) {
+                Search.comparisonCounter++; // Increment the comparison counter
+                low = mid + 1; // Move to the right half of the array
+            } else {
+                high = mid - 1; // Move to the left half of the array
+            }
+        }
+
+        return -1; // Return -1 if the target is not found in the array
+    }
 }
--- ICS4U-ExampleCode/src/unit2StarterCode/Sort.java
+++ ICS4U-ExampleCode/src/unit2StarterCode/Sort.java
@@ -1,150 +1,247 @@
 package unit2StarterCode;
 
+
 import java.util.Arrays;
 import java.util.Scanner;
-
 /**
- * This is a starter class for the Sorting Unit. This class will contain all of the methods that will
- * sort a list using various algorithms.
- * Sept 9, 2023
- * @author Christina Kemp
- *
+* This is a starter class for the Sorting Unit. This class will contain all of the methods that will
+* sort a list using various algorithms.
+* Sept 9, 2023
+* @author Christina Kemp
+*
+*/
+/**
+ * The Sort class provides various sorting algorithms including Bubble Sort,
+ * Insertion Sort, Selection Sort, Quick Sort, and Merge Sort. It also tracks 
+ * the number of swaps, loops, and comparisons made during sorting for efficiency testing.
  */
 public class Sort {
 
-	// these variables will be used for efficiency testing
-	public static int swapCounter;
-	public static int loopCounter;
-	public static int comparisonCounter;
+    // Counters to track the number of swaps, loops, and comparisons
+    public static long swapCounter;
+    public static long loopCounter;
+    public static long comparisonCounter;
 
-	/**
-	 * The entry point to the program for testing all Sorting methods
-	 * @param args unused
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		//Get array values from the user
-		System.out.print("How many numbers would you like: ");
-		int n = sc.nextInt();
-		int[] array = new int[n];
-		
-		System.out.println("Please enter the numbers one at a time.");
-		for(int i = 0; i < n; i++) {
-			array[i] = sc.nextInt();
-		}
-		System.out.println(Arrays.toString(array)); //prints the unsorted list
-			
-		bubble(array);//call your sort method here
-		
-		System.out.println(Arrays.toString(array)); //prints the sorted list
-		sc.close();
-	}
-	
-	/**
-	 * This method will swap the items at indexes i and j in the given array.
-	 * @param arr The array that contains the numbers
-	 * @param i The first index to be swapped
-	 * @param j The second index to be swapped
-	 */
-	private static void swap(int[] arr, int i, int j) {
-		// TODO swap the items at indexes i and j in the given array.
-	}
-	
-	/**
-	 * This method sorts the given array using the bubble sort algorithm
-	 * @param arr The array to be sorted
-	 */
-	public static void bubble(int[] arr) {
-		//TODO write the bubble sort method
-	}
-	
-	/**
-	 * This method sorts the given array using the insertion sort algorithm
-	 * @param arr The array to be sorted
-	 */
-	public static void insertion(int[] arr) {
-		//TODO write the insertion sort method
-	}
+    /**
+     * The entry point for the program to test sorting methods.
+     * Prompts the user to input an array and then calls the sorting method.
+     *
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	/**
-	 * This method sorts the given array using the selection sort algorithm
-	 * @param arr The array to be sorted
-	 */
-	public static void selection(int[] arr) {
-		//TODO write the selection sort method
-	}
-	
-	/**
-	 * This method sorts the given array using the quick sort algorithm.
-	 * @param arr The array to be sorted
-	 */
-	public static void quick(int[] arr) {
-		quick(arr, 0, arr.length - 1);
-	}
-	
-	/**
-	 * This is a method that is used to recursively sort the given array using quick sort
-	 * between the start and end indices
-	 * @param arr The array to be sorted
-	 * @param start The first index of the array to be sorted
-	 * @param end The last index of the array to be sorted
-	 */
-	private static void quick(int[] arr, int start, int end) {
-		//TODO write the quick sort recursive method
-		//base case
-		//partition
-		//quick left
-		//quick right
-	}
-	
-	/**
-	 * This method partitions the given array between the start and end indices
-	 * and returns the index where the final pivot point finished
-	 * @param arr The array to be sorted
-	 * @param start The first index of the array to be sorted
-	 * @param end The last index of the array to be sorted
-	 * @return Returns the index where the pivot is located that divides the list in half
-	 */
-	private static int partition(int[] arr, int start, int end) {
-		//TODO write the partition method
-		return 0;
-	}
-	
-	/**
-	 * This method sorts the given array using the merge sort algorithm.
-	 * @param arr The array to be sorted
-	 */
-	public static void merge(int[] arr) {
-		merge(arr, 0, arr.length - 1);
-	}
-	
-	/**
-	 * This is a method that is used to recursively sort the given array using merge sort
-	 * between the start and end indices
-	 * @param arr The array to be sorted
-	 * @param start The first index of the array to be sorted
-	 * @param end The last index of the array to be sorted
-	 */
-	private static void merge(int[] arr, int start, int end) {
-		//TODO write the merge recursive method
-		//base case		
-		//merge left    start, mid
-		//merge right    mid+1, end
-		//merging         start, end
-	}
-	
-	/**
-	 * This method merges two parts of an array that are each in order into 
-	 * one array that is in order
-	 * @param arr The array to be sorted
-	 * @param start The first index of the array to be sorted
-	 * @param end The last index of the array to be sorted
-	 */
-	private static void merging(int[] arr, int start, int end) {
-		//TODO write the merging method
-		return;
-	}
-	
+        // Get array values from the user
+        System.out.print("How many numbers would you like: ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
 
+        System.out.println("Please enter the numbers one at a time.");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        
+        System.out.println(Arrays.toString(array)); // Prints the unsorted list
+        
+        // Call the bubble sort method
+        bubble(array);
+        
+        System.out.println(Arrays.toString(array)); // Prints the sorted list
+        sc.close();
+    }
+
+    /**
+     * Swaps the elements at indices i and j in the given array.
+     *
+     * @param arr The array containing the elements to be swapped
+     * @param i The first index to be swapped
+     * @param j The second index to be swapped
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        Sort.swapCounter++; // Increment swap counter
+    }
+
+    /**
+     * Sorts the given array using the Bubble Sort algorithm.
+     * This algorithm repeatedly steps through the list, compares adjacent elements, 
+     * and swaps them if they are in the wrong order.
+     *
+     * @param arr The array to be sorted
+     */
+    public static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            Sort.loopCounter++; // Increment loop counter for outer loop
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                Sort.comparisonCounter++; // Increment comparison counter
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Sorts the given array using the Insertion Sort algorithm.
+     * The algorithm builds the sorted array one element at a time by picking 
+     * elements from the unsorted portion and inserting them into the correct position.
+     *
+     * @param arr The array to be sorted
+     */
+    public static void insertion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Sort.loopCounter++; // Increment loop counter for outer loop
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                Sort.comparisonCounter++; // Increment comparison counter
+                swap(arr, j, j - 1); // Swap elements if they are out of order
+                j--;
+            }
+        }
+    }
+
+    /**
+     * Sorts the given array using the Selection Sort algorithm.
+     * The algorithm repeatedly finds the minimum element from the unsorted portion 
+     * and swaps it with the first unsorted element.
+     *
+     * @param arr The array to be sorted
+     */
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            Sort.loopCounter++; // Increment loop counter for outer loop
+            int temp = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                Sort.loopCounter++; // Increment loop counter for inner loop
+                if (arr[j] < arr[temp]) {
+                    Sort.comparisonCounter++; // Increment comparison counter
+                    temp = j;
+                }
+            }
+            // Swap if a smaller element was found
+            if (temp != i) {
+                swap(arr, i, temp);
+            }
+        }
+    }
+
+    /**
+     * Sorts the given array using the Quick Sort algorithm.
+     * Quick Sort is a divide-and-conquer algorithm that recursively partitions 
+     * the array into smaller subarrays and sorts them.
+     *
+     * @param arr The array to be sorted
+     */
+    public static void quick(int[] arr) {
+        quick(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * Helper method to recursively perform Quick Sort on the array.
+     *
+     * @param arr The array to be sorted
+     * @param start The starting index of the subarray to be sorted
+     * @param end The ending index of the subarray to be sorted
+     */
+    private static void quick(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivot = partition(arr, start, end);
+            quick(arr, start, pivot - 1); // Recursively sort the left subarray
+            quick(arr, pivot + 1, end);   // Recursively sort the right subarray
+        }
+    }
+
+    /**
+     * Partitions the array into two halves and returns the pivot index.
+     * The pivot element is placed in the correct sorted position.
+     *
+     * @param arr The array to be partitioned
+     * @param start The starting index of the subarray to be partitioned
+     * @param end The ending index of the subarray to be partitioned
+     * @return The index of the pivot element
+     */
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];  // Select the middle element as pivot
+        swap(arr, (start + end) / 2, end);   // Move the pivot to the end
+        int i = start - 1;
+
+        for (int j = start; j < end; j++) {
+            Sort.loopCounter++; // Increment loop counter
+            if (arr[j] <= pivot) {
+                Sort.comparisonCounter++; // Increment comparison counter
+                i++;
+                swap(arr, i, j); // Swap if the element is less than or equal to the pivot
+            }
+        }
+        swap(arr, i + 1, end); // Move the pivot to its correct position
+        return i + 1;
+    }
+
+    /**
+     * Sorts the given array using the Merge Sort algorithm.
+     * Merge Sort is a divide-and-conquer algorithm that recursively splits the 
+     * array into smaller parts, sorts them, and merges them back together.
+     *
+     * @param arr The array to be sorted
+     */
+    public static void merge(int[] arr) {
+        merge(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * Helper method to recursively perform Merge Sort on the array.
+     *
+     * @param arr The array to be sorted
+     * @param start The starting index of the subarray to be sorted
+     * @param end The ending index of the subarray to be sorted
+     */
+    private static void merge(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start) / 2;
+            merge(arr, start, mid);       // Recursively sort the left half
+            merge(arr, mid + 1, end);     // Recursively sort the right half
+            merging(arr, start, mid, end); // Merge the two sorted halves
+        }
+    }
+
+    /**
+     * Merges two sorted subarrays into one sorted array.
+     *
+     * @param arr The array to be merged
+     * @param start The starting index of the subarray
+     * @param mid The middle index where the array is split
+     * @param end The ending index of the subarray
+     */
+    private static void merging(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+
+        // Merge the two halves into the temp array
+        while (i <= mid && j <= end) {
+            Sort.comparisonCounter++; // Increment comparison counter
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        // Copy the remaining elements of the left half
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // Copy the remaining elements of the right half
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy the sorted elements back into the original array
+        for (k = 0; k < temp.length; k++) {
+            Sort.loopCounter++; // Increment loop counter
+            arr[start + k] = temp[k];
+        }
+    }
 }
